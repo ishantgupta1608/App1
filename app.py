061adb3302161f386bdc6655ab3808cd0c76a566
ishantgupta1608 <ishantgupta1608@gmail.com>
2020-03-25 00:15:06 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -23,6 +23,40 @@ def save():
 def hello(): 
     return str(os.listdir(os.path.join(app.root_path, 'Data Files')))
 
+@app.route('/upload_file')
+def upload_file():
+    html = """
+    <form action = "/upload_success" method = "post" enctype="multipart/form-data">
+        <input type = "file" name = "file_name">
+        <input type = "submit" name = "Upload">
+    </form>
+    """
+    return html
+
+@app.route('/upload_success', methods = ['POST'])
+def upload_success():
+    file = request.files['file_name']
+    file.save(os.path.join(app.root_path, 'Uploads', file.filename))
+    return file.filename + ' uploaded succesfully'
+    #return str(os.listdir(os.path.join(app.root_path, 'Uploads')))
+
+@app.route('/display_files')
+def display_files():
+    html = """
+    <form action = "/download_file" method = "post">
+        <input type = "text" name = "file_name">
+        <input type = "submit" name = "Download">
+    </form>
+    
+    """
+    return html + str(os.listdir(os.path.join(app.root_path, 'Uploads')))
+
+@app.route('/download_file', methods = ['POST'])
+def download_file():
+    return send_file(os.path.join(app.root_path, 'Uploads', 
+                                  request.form['file_name']),  
+                     as_attachment = True)
+    
 if __name__ == '__main__':
     #print(os.path.join(app.instance_path, "Data Files", '1'))
     print(app.instance_path)
