cf36566675ba18eb3f1850f5db35ea59b18e0578
ishantgupta1608 <ishantgupta1608@gmail.com>
2020-03-24 20:38:29 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('hello')
-def index():
+@app.route('/hello')
+def hello():
     return "Hello"
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
