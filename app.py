52a23a2e0f149021fecf63c64af8d20f9753243b
ishantgupta1608 <ishantgupta1608@gmail.com>
2020-03-24 21:49:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///test.db'
+#db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -10,11 +14,15 @@ def index():
 def save():
     name = request.form['Name']
     number = request.form['Number']
+    #file = open(os.path.join("Data Files", name), 'a')
+    #file.append(str(number) + '\n')
     return name + ' : ' + str(number)
 
-@app.route('/hello')
+@app.route('/hello') 
 def hello():
-    return "Hello"
+    return app.instance_path
 
 if __name__ == '__main__':
+    #print(os.path.join(app.instance_path, "Data Files", '1'))
+    print(app.instance_path)
     app.run()
